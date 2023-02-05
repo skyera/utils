3f4626a4b80201469b848c582486c861425e156e
skyera <zgliu2@gmail.com>
2023-02-05 13:18:57 -0800

    refactor dotag2

--- dotag2.py
+++ dotag2.py
@@ -4,107 +4,113 @@ import sys
 import os
 import time
 
-print(sys.version_info)
-print(sys.version)
 
-file_type_list = ('.c', '.cpp', '.cc', '.h', '.hpp', '.inl', '.cs', '.java', '.mc', '.rc', '.idl', '.js', '.ts', '.html', '.py', '.sql', '.sh', '.json')
-
-
-ignore_path_list = ['boost', 'omniorb', 'generated', '_output', '.jazz', 
-                    'node_modules', 'wabapp', 'ucm_components', 
-                    'pythonstandardlibrary', '.vscode',
-                    'virtualenv', '3rdParty','omniwin', 
-                    'openthreads']
+FILE_EXTS = ('.c', '.cpp', '.cc', '.h', '.hpp', '.inl', '.cs', '.java', '.mc',
+             '.rc', '.idl', '.js', '.ts', '.html', '.py', '.sql', '.sh', '.json')
+EXCLUDED_DIRS = ['boost', 'omniorb', 'generated', '_output', '.jazz', 
+               'node_modules', 'wabapp',
+               'pythonstandardlibrary', '.vscode',
+               'virtualenv', '3rdParty','omniwin', 
+               'openthreads']
+CSCOPE_FILE_NAME = "cscope.files"
+FILENAMETAG_FILE_NAME = "filenametags"
 
 
 def parse():
     parser = argparse.ArgumentParser(prog='dotag.py',
-            description="generate cscope/tags data",
-            epilog="speed")
+                description="generate cscope/tags data",
+                epilog="fast search")
     parser.add_argument("-f", '--find', help='use gnu find', action='store_true')
     args = parser.parse_args()
     return args.find
 
 
-def ignore_dirpath(dirpath):
-    path = dirpath.lower()
-    for ignore_path in ignore_path_list:
-        if ignore_path in path:
+def is_excluded(dir_name):
+    dir_name = dir_name.lower()
+    for exclude_dir in EXCLUDED_DIRS:
+        if exclude_dir in dir_name:
             return True
 
     return False
 
 
-def visit(arg, dirpath, namelist):
-    if ignore_dirpath(dirpath):
+def visit(files, dirpath, file_names):
+    if is_excluded(dirpath):
         return
 
-    for name in namelist:
-        path = os.path.join(dirpath, name)
+    for file_name in file_names:
+        path = os.path.join(dirpath, file_name)
         if os.path.isfile(path) and not os.path.islink(path):
-            root, ext = os.path.splitext(name)
-            if ext.lower() in file_type_list:
+            root, ext = os.path.splitext(file_name)
+            if ext.lower() in FILE_EXTS:
                 if ' ' not in path:
-                    arg.append(path)
+                    files.append(path)
                 else:
                     print(path, 'has spaces')
 
 
 class FindCmd:
-    def __init__(self, ignore_paths, file_exts):
-        self.ignore_paths = ignore_paths
+    """Generate find command by using find"""
+    def __init__(self, excluded_dirs, file_exts):
+        self.excluded_dirs = excluded_dirs
         self.file_exts = file_exts
-        self.ignore_paths_str = ''
+        self.excluded_dirs_str = ''
+        self.file_exts_str = ''
         self.find_cmd = ''
 
     def create(self):
-        self.generate_ignore_paths_str()
-        self.generate_files_types_str()
+        self.generate_excluded_dirs_str()
+        self.generate_file_exts_str()
         self.generate_find_cmd()
         print(self.find_cmd)
 
-    def generate_ignore_paths_str(self):
-        if not self.ignore_paths:
+    def generate_excluded_dirs_str(self):
+        if not self.excluded_dirs:
             return
 
         paths = []
-        for path in self.ignore_paths:
+        for path in self.excluded_dirs:
             paths.append(f'-iname "*{path}*"')
-        self.ignore_paths_str = ' -or '.join(paths)
-        self.ignore_paths_str = f'-type d \( {self.ignore_paths_str} \) -prune'
+        self.excluded_dirs_str = ' -or '.join(paths)
+        self.excluded_dirs_str = f'-type d \( {self.excluded_dirs_str} \) -prune'
 
-    def generate_files_types_str(self):
+    def generate_file_exts_str(self):
         items = []
         for ext in self.file_exts:
             items.append(f'-iname "*{ext}"')
-        self.file_types_str = ' -or '.join(items)
-        self.file_types_str = f'-type f \( {self.file_types_str} \)'
+        self.file_exts_str = ' -or '.join(items)
+        self.file_exts_str = f'-type f \( {self.file_exts_str} \)'
 
     def generate_find_cmd(self):
-        if self.ignore_paths_str != "":
-            self.find_cmd = f'find . {self.ignore_paths_str} -or {self.file_types_str} -fprint cscope.files'
+        if self.excluded_dirs_str != "":
+            self.find_cmd = f'find . {self.excluded_dirs_str} -or {self.file_exts_str} -fprint {CSCOPE_FILE_NAME}'
         else:
-            self.find_cmd = f'find . {self.file_types_str} -fprint cscope.files'
+            self.find_cmd = f'find . {self.file_exts_str} -fprint {CSCOPE_FILE_NAME} '
 
 
 def get_files():
     myfiles = []
-    for root, dirs, files in os.walk("."):
-        visit(myfiles, root, files)
+    for root, dirs, file_names in os.walk("."):
+        visit(myfiles, root, file_names)
     return myfiles
 
 
 def gnu_find_files():
-    cmd = FindCmd(ignore_path_list, file_type_list)
+    cmd = FindCmd(EXCLUDED_DIRS, FILE_EXTS)
     cmd.create()
     os.system(cmd.find_cmd)
 
 
 def py_find_files():
     files = get_files()
-    with open('cscope.files', 'w') as f:
+    with open(CSCOPE_FILE_NAME, 'w') as f:
         for fname in files:
             f.write('%s\n' % fname)
+
+
+def log_cpu(msg, start):
+    cpu = time.time() - start
+    print(msg, 'CPU', cpu, 'seconds')
 
 
 def collect_files(use_gnu_find):
@@ -114,83 +120,89 @@ def collect_files(use_gnu_find):
         gnu_find_files()
     else:
         py_find_files()
-    end = time.time()
-    cpu = end - start
-    print('find files', get_number_files(), 'cpu:', cpu, 'seconds')
+
+    num_files = get_number_files()
+    log_cpu(f'find files: number of files {num_files}', start)
 
 
 def run_cscope():
     start = time.time()
-    cmd = 'cscope -b -q -k -i cscope.files'
+    cmd = f'cscope -b -q -k -i {CSCOPE_FILE_NAME}'
     print(cmd)
     os.system(cmd)
-    end = time.time()
-    cpu = end - start
-    print('cscope cpu', cpu, 'seconds')
+    log_cpu('cscope', start)
 
 
-def get_number_files(filename='cscope.files'):
+def get_number_files(filename=CSCOPE_FILE_NAME):
     with open(filename, "r") as f:
         lines = f.readlines()
     return len(lines)
 
 
-def create_tempfile():
-    cscope_f = open('cscope.files')
-    temp_f = open('temp.txt', 'w')
-    for line in cscope_f:
-        path = line.strip('"\n')
-        name = os.path.basename(path)
-        s = '%s\t%s\t1' % (name, path)
-        temp_f.write(s + '\n')
-    cscope_f.close()
-    temp_f.close()
-
-
 def create_filenametags():
-    create_tempfile()
-    add_tag()
-    sort_filenametags()
-    os.remove('temp.txt')
+    creator = FilenametagsCreator(CSCOPE_FILE_NAME, FILENAMETAG_FILE_NAME)
+    creator.run()
 
 
-def add_tag():
-    with open('filenametags', 'w') as tag_f:
-        tag_f.write('!_TAG_FILE_SORTED\t2\t/2=foldcase/\n')
+class FilenametagsCreator:
+    def __init__(self, cscope_filename, tag_filename):
+        self.cscope_filename = cscope_filename
+        self.temp_filename = 'temp.txt'
+        self.tag_filename = tag_filename
 
+    def run(self):
+        self.create_tagfile()
+        self.create_tempfile()
+        self.sort_append_file()
+        os.remove(self.temp_filename)
+    
+    def create_tempfile(self):
+        cscope_f = open(self.cscope_filename)
+        temp_f = open(self.temp_filename, 'w')
+        for line in cscope_f:
+            path = line.strip('"\n')
+            name = os.path.basename(path)
+            s = '%s\t%s\t1' % (name, path)
+            temp_f.write(s + '\n')
+        cscope_f.close()
+        temp_f.close()
+    
+    def create_tagfile(self):
+        with open(self.tag_filename, 'w') as tag_f:
+            tag_f.write('!_TAG_FILE_SORTED\t2\t/2=foldcase/\n')
 
-def sort_filenametags():
-    cmd = 'sort -f temp.txt >> filenametags'
-    print(cmd)
-    os.system(cmd)
+    def sort_append_file(self):
+        cmd = f'sort -f {self.temp_filename} >> {self.tag_filename}'
+        print(cmd)
+        os.system(cmd)
 
 
 def create_tags():
     start = time.time()
-    cmd = 'ctags -L cscope.files'
+    cmd = f'ctags -L {CSCOPE_FILE_NAME}'
     print(cmd)
     os.system(cmd)
-    end = time.time()
-    cpu = end - start
-    print('ctags cpu', cpu, 'seconds')
+    log_cpu("ctags", start)
 
 
-def main():
-    use_gnu_find = parse()
+def print_find(use_gnu_find):
     if use_gnu_find:
         print('Gnu find')
     else:
         print('py find')
-    start = time.time()
+
+
+def main():
+    use_gnu_find = parse()
+    print_find(use_gnu_find)
     
+    start = time.time()
     collect_files(use_gnu_find)
     run_cscope() 
     create_filenametags()
     create_tags()
 
-    end = time.time()
-    cpu = end - start
-    print('cpu', cpu, 'seconds')
+    log_cpu('Total', start)
 
 
 if __name__ == '__main__':
