8872246ff1d1624e5851583771ca0429382ce019
skyera <skyera@users.noreply.github.com>
2025-05-29 08:59:09 -0700

    chafa view image

--- lf-preview.sh
+++ lf-preview.sh
@@ -1,2 +1,9 @@
 #!/bin/sh
-bat --style="plain,numbers" --color=always --paging=never "$1"
+case "$1" in
+    *.png|*.jpg|*.jpeg|*.gif|*.bmp)
+        chafa --size=40x20 --colors=full "$1"
+        ;;
+    *)
+        bat --style="plain,numbers" --color=always --paging=never "$1"
+        ;;
+esac
