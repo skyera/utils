6e255395215e64e3e0fd059f6718365bb0c8f0ff
skyera <skyera@users.noreply.github.com>
2025-05-29 09:21:30 -0700

    Update lf-preview.bat chafa

--- lf-preview.bat
+++ lf-preview.bat
@@ -1,5 +1,21 @@
 @echo off
-REM Use bat to preview the file with paging and syntax highlighting
+REM Use bat for text files and chafa for images
 REM lf passes the file path as the first argument
 
-bat --style="plain,numbers" --color=always --paging=never "%1"
+set "FILE=%1"
+
+REM Check file extension (case-insensitive)
+if /i "%FILE:~-4%"==".png"  goto :image
+if /i "%FILE:~-4%"==".jpg"  goto :image
+if /i "%FILE:~-5%"==".jpeg" goto :image
+if /i "%FILE:~-4%"==".gif"  goto :image
+if /i "%FILE:~-4%"==".bmp"  goto :image
+
+REM Default to bat for text files
+bat --style="numbers" --color=always --paging=never "%FILE%" 2> bat-error.log
+goto :eof
+
+:image
+REM Use chafa for image files
+chafa --size=40x20 --colors=full "%FILE%"
+goto :eof
