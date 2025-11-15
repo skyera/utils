81d84ac2ce737f68e9820bdfd628f5f03c1d6262
zliu <zgliu2@gmail.com>
2025-11-14 23:37:45 -0800

    print logfile

--- run-val.sh
+++ run-val.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 ts=$(date +"%Y%m%d_%H%M%S")
-VALGRIND_OPTS="--leak-check=full --track-origins=yes --show-leak-kinds=all --log-file=valgrind_${ts}.log"
+LOGFILE="valgrind_${ts}.log"
+echo "Log file: $LOGFILE"
+VALGRIND_OPTS="--leak-check=full --track-origins=yes --show-leak-kinds=all --log-file=$LOGFILE"
 exec valgrind $VALGRIND_OPTS "$@"
-
