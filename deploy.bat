d0458092945bcc02bca3ea0cbe1a01002004abff
skyera <zgliu2@gmail.com>
2026-04-22 11:11:41 -0700

    Configure Windows environment variables for fzf and ripgrep

--- deploy.bat
+++ deploy.bat
@@ -8,28 +8,39 @@ set "REPO_DIR=%REPO_DIR:~0,-1%"
 echo Deploying for Windows...
 
 :: 1. Deploy binaries to C:\app\bin
-echo [1/2] Deploying binaries to C:\app\bin...
+echo [1/2] Deploying binaries...
 if not exist "C:\app\bin" mkdir "C:\app\bin"
 xcopy /Y /S /E "%REPO_DIR%\bin\*" "C:\app\bin\"
 
-:: 2. Deploy lf configuration
-echo [2/2] Deploying application configurations...
-set "LF_CONFIG_DIR=%LOCALAPPDATA%\lf"
-if not exist "%LF_CONFIG_DIR%" mkdir "%LF_CONFIG_DIR%"
-set "FD_CONFIG_DIR=%APPDATA%\fd"
-if not exist "%FD_CONFIG_DIR%" mkdir "%FD_CONFIG_DIR%"
+:: 2. Deploy application configurations
+echo [2/2] Deploying configurations...
 
-:: Copy configs
-copy /Y "%REPO_DIR%\.config\lf\lfrc_windows" "%LF_CONFIG_DIR%\lfrc"
-if exist "%REPO_DIR%\.config\lf\icons" copy /Y "%REPO_DIR%\.config\lf\icons" "%LF_CONFIG_DIR%\icons"
-copy /Y "%REPO_DIR%\.config\fd\ignore" "%FD_CONFIG_DIR%\ignore"
-copy /Y "%REPO_DIR%\.ripgreprc" "%USERPROFILE%\.ripgreprc"
+:: Helper to deploy a file (creates directory and copies)
+call :deploy_file "%REPO_DIR%\.config\lf\lfrc_windows" "%LOCALAPPDATA%\lf\lfrc"
+call :deploy_file "%REPO_DIR%\.config\lf\icons"        "%LOCALAPPDATA%\lf\icons"
+call :deploy_file "%REPO_DIR%\.config\fd\ignore"      "%APPDATA%\fd\ignore"
+call :deploy_file "%REPO_DIR%\.ripgreprc"             "%USERPROFILE%\.ripgreprc"
 
-:: Set Ripgrep Environment Variable
-echo Setting RIPGREP_CONFIG_PATH...
+:: Set Environment Variables
+echo Setting Environment Variables...
 setx RIPGREP_CONFIG_PATH "%USERPROFILE%\.ripgreprc"
+setx FZF_DEFAULT_COMMAND "fd --follow --hidden --exclude .git --ignore-file \"%APPDATA%\fd\ignore\""
+setx FZF_DEFAULT_OPTS "--preview \"bat --color=always {}\""
 
 echo.
 echo Deployment complete!
-echo Note: Ensure C:\app\bin is in your System PATH to use the tools globally.
+echo Note: Environment variables will take effect in NEW terminal windows.
+echo Note: Ensure C:\app\bin is in your System PATH.
 pause
+goto :eof
+
+:deploy_file
+set "src=%~1"
+set "dest=%~2"
+if exist "%src%" (
+    for %%I in ("%dest%") do set "dest_dir=%%~dpI"
+    if not exist "!dest_dir!" mkdir "!dest_dir!"
+    copy /Y "%src%" "%dest%"
+    echo Deployed %dest%
+)
+goto :eof
