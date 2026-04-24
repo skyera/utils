4ac3ab8f1500093cac6c044d2265bec74550a37c
skyera <zgliu2@gmail.com>
2026-04-24 02:33:08 -0700

    Make Neovim config style selection optional in deploy.bat

--- deploy.bat
+++ deploy.bat
@@ -21,12 +21,30 @@ call :deploy_file "%REPO_DIR%\.config\lf\icons"        "%LOCALAPPDATA%\lf\icons"
 call :deploy_file "%REPO_DIR%\.config\fd\ignore"      "%APPDATA%\fd\ignore"
 call :deploy_file "%REPO_DIR%\.ripgreprc"             "%USERPROFILE%\.ripgreprc"
 call :deploy_file "%REPO_DIR%\myvimrc"                "%USERPROFILE%\_vimrc"
-:: Deploy Neovim Lua configuration
-if exist "%REPO_DIR%\.config\nvim" (
-    if not exist "%LOCALAPPDATA%\nvim" mkdir "%LOCALAPPDATA%\nvim"
-    if exist "%LOCALAPPDATA%\nvim\init.vim" del /Q "%LOCALAPPDATA%\nvim\init.vim"
-    xcopy /Y /S /E "%REPO_DIR%\.config\nvim\*" "%LOCALAPPDATA%\nvim\"
-    echo Deployed Neovim configuration to %LOCALAPPDATA%\nvim
+
+:: Neovim configuration selection
+echo.
+echo Select Neovim configuration style:
+echo 1) Lua (Modern, faster, separate config)
+echo 2) Vimscript (Legacy, uses myvimrc/init.vim)
+set /p NVIM_CHOICE="Enter choice [1-2]: "
+
+if "%NVIM_CHOICE%"=="1" (
+    :: Deploy Neovim Lua configuration
+    if exist "%REPO_DIR%\.config\nvim" (
+        if not exist "%LOCALAPPDATA%\nvim" mkdir "%LOCALAPPDATA%\nvim"
+        if exist "%LOCALAPPDATA%\nvim\init.vim" del /Q "%LOCALAPPDATA%\nvim\init.vim"
+        xcopy /Y /S /E "%REPO_DIR%\.config\nvim\*" "%LOCALAPPDATA%\nvim\"
+        echo Deployed Neovim configuration to %LOCALAPPDATA%\nvim
+    )
+) else if "%NVIM_CHOICE%"=="2" (
+    echo Deploying Neovim Vimscript configuration...
+    :: Remove Lua config directory if it exists to avoid conflicts
+    if exist "%LOCALAPPDATA%\nvim\lua" rd /S /Q "%LOCALAPPDATA%\nvim\lua"
+    if exist "%LOCALAPPDATA%\nvim\init.lua" del /Q "%LOCALAPPDATA%\nvim\init.lua"
+    call :deploy_file "%REPO_DIR%\myvimrc" "%LOCALAPPDATA%\nvim\init.vim"
+) else (
+    echo Invalid choice. Skipping Neovim configuration.
 )
 
 :: Set Environment Variables
