02e21059a38d2f38056c2a58cf7d0a37e0408f83
skyera <zgliu2@gmail.com>
2026-04-24 07:45:17 -0700

    remove nvim-treesitter due to installation errors

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -89,18 +89,6 @@ return {
   "tpope/vim-fugitive",
   "jiangmiao/auto-pairs",
   "godlygeek/tabular",
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-      require("nvim-treesitter.configs").setup({
-        ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "python", "javascript", "typescript", "html" },
-        sync_install = false,
-        highlight = { enable = true },
-        indent = { enable = true },
-      })
-    end,
-  },
   "christoomey/vim-tmux-navigator",
   "tmhedberg/SimpylFold",
   "majutsushi/tagbar",
