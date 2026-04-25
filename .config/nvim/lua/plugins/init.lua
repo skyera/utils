dda3055c38948086a5ba617083a56c3e1fe8db71
zliu <zgliu71@gmail.com>
2026-04-25 09:56:30 -0700

    feat(nvim): add nvim-treesitter for improved syntax highlighting

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -90,6 +90,24 @@ return {
     end,
   },
 
+  -- Treesitter
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "python", "javascript", "typescript", "html", "css" },
+        sync_install = false,
+        auto_install = true,
+        highlight = {
+          enable = true,
+          additional_vim_regex_highlighting = false,
+        },
+        indent = { enable = true },
+      })
+    end,
+  },
+
   -- Coding
   {
     "dhananjaylatkar/cscope_maps.nvim",
