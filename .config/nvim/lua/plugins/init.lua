212eb81b9a685a27ff51df391182f776d9649355
skyera <zgliu2@gmail.com>
2026-04-26 09:59:39 -0700

    feat(nvim): add oil.nvim for buffer-based file management

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -54,6 +54,19 @@ return {
     end,
   },
 
+  -- File Explorer (Oil)
+  {
+    "stevearc/oil.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      require("oil").setup({
+        columns = { "icon" },
+        view_options = { show_hidden = true },
+      })
+      vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+    end,
+  },
+
   -- Navigation (Classic Tools)
   "scrooloose/nerdtree",
   {
