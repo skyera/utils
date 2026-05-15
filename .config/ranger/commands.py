d17beb9dc313424465e6b2234978d952c7f64a04
Zhigang <zgliu71@gmail.com>
2026-05-15 10:24:53 -0700

    feat(ranger): enhance FZF commands with live previews and switch fl to fd-based home search

--- .config/ranger/commands.py
+++ .config/ranger/commands.py
@@ -11,9 +11,9 @@ class fzf_select(Command):
         import subprocess
         # Using 'fd' for speed and .gitignore respect, matching your bashrc preferences
         if self.quantifier:
-            command="fd --type d --hidden --exclude .git | fzf +m"
+            command="fd --type d --hidden --exclude .git | fzf +m --preview 'bat --color=always --style=numbers --line-range :500 {}'"
         else:
-            command="fd --hidden --exclude .git | fzf +m"
+            command="fd --hidden --exclude .git | fzf +m --preview 'bat --color=always --style=numbers --line-range :500 {}'"
             
         fzf = self.fm.execute_command(command, stdout=subprocess.PIPE)
         stdout, stderr = fzf.communicate()
@@ -27,15 +27,18 @@ class fzf_select(Command):
 class fzf_locate(Command):
     """
     :fzf_locate
-    Find a file using fzf and locate.
+    Find a file using fzf and fd starting from the home directory.
     """
     def execute(self):
         import subprocess
-        command="locate home media | fzf -e -i"
+        import os
+        home = os.path.expanduser('~')
+        # Search from home, but we'll use a relative path for fzf display
+        command = f"fd --base-directory {home} --hidden --exclude .git | fzf -e -i --preview 'bat --color=always --style=numbers --line-range :500 {home}/{{}}'"
         fzf = self.fm.execute_command(command, stdout=subprocess.PIPE)
         stdout, stderr = fzf.communicate()
         if fzf.returncode == 0:
-            fzf_file = os.path.abspath(stdout.decode('utf-8').rstrip('\n'))
+            fzf_file = os.path.join(home, stdout.decode('utf-8').rstrip('\n'))
             if os.path.isdir(fzf_file):
                 self.fm.cd(fzf_file)
             else:
