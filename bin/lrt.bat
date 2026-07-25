d6c465790e638516234342be8c19ded8a56b6f8b
skyera <zgliu2@gmail.com>
2026-07-25 10:54:05 -0700

    fix(lrt): remove --group-directories-first from lrt alias

--- bin/lrt.bat
+++ bin/lrt.bat
@@ -1,7 +1,7 @@
 @echo off
 where eza >nul 2>nul
 if %errorlevel% equ 0 (
-    eza -lh -s newest --icons=always --group-directories-first %*
+    eza -lh -s newest --icons=always %*
 ) else (
     where ls >nul 2>nul
     if %errorlevel% equ 0 (
