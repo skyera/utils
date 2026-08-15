2f2d81a573bf6b7f9da613adb740eaec446712cd
skyera <zgliu2@gmail.com>
2026-08-15 09:34:26 -0700

    feat(nvim): add lewis6991/gitsigns.nvim with hunk keymaps

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -292,6 +292,47 @@ return {
   "tpope/vim-unimpaired",
   "tpope/vim-fugitive",
   {
+    "lewis6991/gitsigns.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    opts = {
+      signs = {
+        add          = { text = "│" },
+        change       = { text = "│" },
+        delete       = { text = "_" },
+        topdelete    = { text = "‾" },
+        changedelete = { text = "~" },
+      },
+      on_attach = function(bufnr)
+        local gs = package.loaded.gitsigns
+        local function map(mode, l, r, desc)
+          vim.keymap.set(mode, l, r, { buffer = bufnr, silent = true, desc = "Git: " .. desc })
+        end
+
+        -- Navigation
+        map("n", "]c", function()
+          if vim.wo.diff then return "]c" end
+          vim.schedule(function() gs.next_hunk() end)
+          return "<Ignore>"
+        end, "Next Hunk")
+        map("n", "[c", function()
+          if vim.wo.diff then return "[c" end
+          vim.schedule(function() gs.prev_hunk() end)
+          return "<Ignore>"
+        end, "Prev Hunk")
+
+        -- Actions
+        map("n", "<leader>hs", gs.stage_hunk, "Stage Hunk")
+        map("n", "<leader>hr", gs.reset_hunk, "Reset Hunk")
+        map("v", "<leader>hs", function() gs.stage_hunk({ vim.fn.line("."), vim.fn.line("v") }) end, "Stage Hunk (Visual)")
+        map("v", "<leader>hr", function() gs.reset_hunk({ vim.fn.line("."), vim.fn.line("v") }) end, "Reset Hunk (Visual)")
+        map("n", "<leader>hp", gs.preview_hunk, "Preview Hunk Diff")
+        map("n", "<leader>hb", function() gs.blame_line({ full = true }) end, "Blame Line (Popup)")
+        map("n", "<leader>tb", gs.toggle_current_line_blame, "Toggle Line Blame")
+        map("n", "<leader>hd", gs.diffthis, "Diff This Buffer")
+      end,
+    },
+  },
+  {
     "windwp/nvim-autopairs",
     event = "InsertEnter",
     config = true,
